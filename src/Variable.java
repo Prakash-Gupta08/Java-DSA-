8884079735a8411123d6ab482e9f6c77fb9a3281
Prakash-Gupta08 <guptaprakash0809@gmail.com>
2026-08-17 13:26:29 +0530

    Add new class variable

---
+++ src/Variable.java
@@ -0,0 +1,28 @@
+public class Variable {
+    public  static  void main()
+    {
+        //System.out.println("age");
+        //create, declare
+        int age = 11;
+        System.out.println("age");
+        //assign value
+        age = 13;
+        age = 19;
+        System.out.println(age);
+
+        //define or initialize
+        int totalMarks = 20;
+        System.out.println(totalMarks);
+
+        //case sensitive
+        int weight = 50;
+        int WEIGHT = 79;
+        System.out.println("wieght:" + weight);
+        System.out.println("WEIGHT" + WEIGHT);
+
+    }
+            
+
+    
+    
+}
--- src/magiee.java
+++ src/magiee.java
@@ -3,4 +3,11 @@ public class magiee {
     {
         System.out.println("Hey! This is 2026.");
     }
+
+    /*
+    Variable :
+    int age = 5;
+    int is type of the data which is integer, this is the datatype and the age is variable(named memory allocation)
+    = : is a assignment operator.
+    */
 }
