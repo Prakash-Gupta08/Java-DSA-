a642ef8c95e7069813bea398242754bd2a68d2ba
Prakash-Gupta08 <guptaprakash0809@gmail.com>
2026-08-18 11:37:58 +0530

    Premimtive DT.

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,7 +1,7 @@
 public class DataTypes {
     public static void main()
     {
-        //Numeric DT- short, byte, int, long
+        // 1.Numeric DT- short, byte, int, long
         byte num1 = 126;
         System.out.println(num1);
 
@@ -9,6 +9,34 @@ public class DataTypes {
         byte num2 = 500; // this give error because byte have range is (-128 to 127)
         System.out.println(num1);
         */
+
+        short num2 = 500;
+        System.out.println(num2);
+
+        /*
+        short num3 = 50000; // This give error because num2 has range -32768 to 32767
+        System.out.println(num3);
+        */
+
+        int num3 = 43000;
+        long num4 = 34000;
+        System.out.println(num3);
+        System.out.println(num4);
+
+
+        //2. Floating DT - float, double
+        float num5 = 3.141315f;
+        System.out.println(num5);
+
+        double num6 = 3.454647484912343456;
+        System.out.println(num6);
+
+        //Other - char, boolean
+        boolean toVote = true;
+        System.out.println(toVote);
+
+        char fitstCharacter = 'a';
+        System.out.println("My first character is : " + fitstCharacter);
 
     }
 }
