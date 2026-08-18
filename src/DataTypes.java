52e7700d5753aafc68c2dbd9b7772d9fb7d0137e
Prakash-Gupta08 <guptaprakash0809@gmail.com>
2026-08-18 11:22:31 +0530

    Data types and it's properties.

---
+++ src/DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes {
+    public static void main()
+    {
+        //Numeric DT- short, byte, int, long
+        byte num1 = 126;
+        System.out.println(num1);
+
+        /*
+        byte num2 = 500; // this give error because byte have range is (-128 to 127)
+        System.out.println(num1);
+        */
+
+    }
+}
