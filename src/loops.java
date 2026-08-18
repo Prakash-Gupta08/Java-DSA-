5069b9bb25b239e5ffb6afad95660639037bb17f
Prakash-Gupta08 <guptaprakash0809@gmail.com>
2026-08-18 13:16:42 +0530

    Loops concept

---
+++ src/loops.java
@@ -0,0 +1,51 @@
+public class loops {
+    public static void main()
+    {
+//        for(int i= 0; i<=5; i++){
+//            System.out.println("Value of i: "+i);
+//        }
+//        for(int i= 0; i<=5; i++){
+//            System.out.println("CP : Skylabs");
+//        }
+
+        //Print odd number:
+//        for(int i= 1; i<=10; i = i+2){
+//            System.out.println("Value of i: "+i);
+//        }
+
+        //Nested Loops
+//        for(int i=1; i<=3; i++)
+//        {
+//            for(int j=1; j<=3; j++)
+//            {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+//        for(int i=1; i<=3; i++){
+//            for(int j=1; j<=3; j++){
+//                System.out.println("i = " + i + ", j = " +j);
+//            }
+//        }
+
+        //Break And Continue
+//        for(int i = 1; i<=10; i++)
+//        {
+//            if(i ==9 )
+//            {
+//                break;
+//            }
+//            System.out.println(i);
+//        }
+
+        for(int i = 1; i<=10; i++)
+        {
+            if(i ==3 )
+            {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
