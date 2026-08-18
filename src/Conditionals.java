fc968300066fd01e5e880f3ff24477d80367f3c4
Prakash-Gupta08 <guptaprakash0809@gmail.com>
2026-08-18 12:41:29 +0530

    Conditional statement

--- src/Conditionals.java
+++ src/Conditionals.java
@@ -1,4 +1,55 @@
-package PACKAGE_NAME;
-
 public class Conditionals {
+    public static void main()
+    {
+//        int dailyPractice = 12;
+//        if(dailyPractice >= 12)
+//        {
+//            System.out.println("Good consistency");
+//        }
+//        int number = 65;
+//        if(number >= 60)
+//        {
+//            System.out.println("Pass");
+//        }
+//        else {
+//            System.out.println("Fail");
+//        }
+//        int accuracy = 78;
+//        if(accuracy >= 90)
+//        {
+//            System.out.println("Excellent");
+//        } else if (accuracy >=75) {
+//            System.out.println("Good");
+//
+//        } else if (accuracy >= 60) {
+//            System.out.println("Average");
+//
+//        }
+//        else {
+//            System.out.println("needs improvement");
+//        }
+        int day = 3;
+        if(day == 1)
+        {
+            System.out.println("Monday");
+        }else if(day == 2)
+        {
+            System.out.println("Tuesday");
+        }else if(day == 3)
+        {
+            System.out.println("Wednesday");
+        }else if(day == 4)
+        {
+            System.out.println("Thursday");
+        }else if(day == 5)
+        {
+            System.out.println("Friday");
+        }else if(day == 6)
+        {
+            System.out.println("Saturday");
+        }else if(day == 7)
+        {
+            System.out.println("Sunday");
+        }
+    }
 }
--- src/InputOutput.java
+++ src/InputOutput.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.math.BigInteger;
+import java.util.Scanner;
 
 public class InputOutput {
+    public static void main()
+    {
+//        int a =5;
+//        int b = 20;
+//        System.out.println(a+b);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value for the first number : ");
+        int firstNum = sc.nextInt();
+        System.out.println("Enter the value for the second number : ");
+        int secondNum = sc.nextInt();
+        int ans  = firstNum + secondNum;
+        System.out.println("Answer is : " + ans);
+        System.out.println("Enter the biginteger value");
+        BigInteger bg = sc.nextBigInteger();
+        System.out.println("Biginteger:" +bg);
+
+        System.out.println("Enter the flag value");
+        boolean flag = sc.nextBoolean();
+        System.out.println("Answer is : " + flag);
+        System.out.println("Enter the short value");
+        short shortVal = sc.nextShort();
+        System.out.println("Answer is : " + shortVal);
+        System.out.println("Enter the float value");
+        float floatVal = sc.nextFloat();
+        System.out.println("Answer is : " + floatVal);
+    }
 }
