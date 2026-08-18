479c1114fc17bc1df221ce44d095ba5e3f3cd827
Prakash-Gupta08 <guptaprakash0809@gmail.com>
2026-08-18 12:52:59 +0530

    Switch case.

--- src/Conditionals.java
+++ src/Conditionals.java
@@ -1,11 +1,16 @@
+import java.util.Scanner;
+
 public class Conditionals {
     public static void main()
     {
+
 //        int dailyPractice = 12;
 //        if(dailyPractice >= 12)
 //        {
 //            System.out.println("Good consistency");
 //        }
+
+
 //        int number = 65;
 //        if(number >= 60)
 //        {
@@ -14,6 +19,8 @@ public class Conditionals {
 //        else {
 //            System.out.println("Fail");
 //        }
+
+
 //        int accuracy = 78;
 //        if(accuracy >= 90)
 //        {
@@ -28,28 +35,64 @@ public class Conditionals {
 //        else {
 //            System.out.println("needs improvement");
 //        }
-        int day = 3;
-        if(day == 1)
-        {
-            System.out.println("Monday");
-        }else if(day == 2)
-        {
-            System.out.println("Tuesday");
-        }else if(day == 3)
-        {
-            System.out.println("Wednesday");
-        }else if(day == 4)
-        {
-            System.out.println("Thursday");
-        }else if(day == 5)
-        {
-            System.out.println("Friday");
-        }else if(day == 6)
-        {
-            System.out.println("Saturday");
-        }else if(day == 7)
-        {
-            System.out.println("Sunday");
+
+
+//        int day = 6;
+//        if(day == 1)
+//        {
+//            System.out.println("Monday");
+//        }else if(day == 2)
+//        {
+//            System.out.println("Tuesday");
+//        }else if(day == 3)
+//        {
+//            System.out.println("Wednesday");
+//        }else if(day == 4)
+//        {
+//            System.out.println("Thursday");
+//        }else if(day == 5)
+//        {
+//            System.out.println("Friday");
+//        }else if(day == 6)
+//        {
+//            System.out.println("Saturday");
+//        }else if(day == 7)
+//        {
+//            System.out.println("Sunday");
+//        }
+
+
+        //Switch Statement =========================
+        System.out.println("Enter the value of day:");
+        Scanner sc = new Scanner(System.in);
+
+        int day = sc.nextInt();
+        switch (day){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Sunday");
+
+
+
         }
+
+
     }
 }
