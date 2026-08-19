2b3b27c9a2cff00e55a3f9fa19e35d936de5f669
Prakash-Gupta08 <guptaprakash0809@gmail.com>
2026-08-19 12:37:56 +0530

    Add pattern printing code.

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -124,23 +124,23 @@ public class PatternPrinting {
 //            }
 //            System.out.println();
 //        }
-//        int m = 10;
-//        for(int i=1; i<=m; i++) {
-//            if(i==1 || i==2 || i==m){
-//                for(int j=1; j<=i; j++){
-//                    System.out.print("*");
-//                }
-//            }
-//            else {
-//                System.out.print("*");
-//                for(int j=1; j<=(i-2); j++){
-//                    System.out.print(" ");
-//                }
-//                System.out.print("*");
-//            }
-//            System.out.println();
-//
-//        }
+        int m = 10;
+        for(int i=1; i<=m; i++) {
+            if(i==1 || i==2 || i==m){
+                for(int j=1; j<=i; j++){
+                    System.out.print("*");
+                }
+            }
+            else {
+                System.out.print("*");
+                for(int j=1; j<=(i-2); j++){
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
 
         int n = 5;
         for (int i = 1; i <= n; i++) {
