252e4507f3221a3367b274e8b353e9f0f284b7d1
Prakash-Gupta08 <guptaprakash0809@gmail.com>
2026-08-19 14:35:25 +0530

    method concept.

--- src/MethodsInJava.java
+++ src/MethodsInJava.java
@@ -1,6 +1,16 @@
 public class MethodsInJava {
-    public  static void main(){
+    static void main() {
+        System.out.println("Hi..");
+        printTable();
+        System.out.println("Java..");
+    }
 
-
+    static void printTable() {
+        for (int i = 1; i <= 10; i++) {
+            int ans = 2 * i;
+            System.out.println(ans);
+        }
     }
 }
+
+
